068e30cc38912c74ae075c16988d0ce6da48e35a
prince203 <prince20@my.yorku.ca>
2024-09-28 18:13:54 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,18 @@ require('dotenv').config()
 
 const connetDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const dbName = process.env.MONGO_DB_NAME;
+        const username = process.env.MONGO_USERNAME;
+        const password = process.env.MONGO_PASSWORD;
+        const host = process.env.MONGO_HOST;
+        
+        await mongoose.connect(host, {
+            dbName: dbName,
+            user: username,
+            pass: password,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('MongoDB Connected!')
     } catch (error) {
         console.log(error.message);
