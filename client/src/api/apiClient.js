7d827596377b060d650591f4bcf2c7307975cf38
prince203 <prince20@my.yorku.ca>
2024-09-28 16:19:55 -0400

    some fix

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000/api';      // package.json - proxy handles the BE URL
+console.log('BASE_URL:', BASE_URL);
+
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
